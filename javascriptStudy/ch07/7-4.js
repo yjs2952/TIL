d2298cf5569b1d9e19bf61927a58d7ad991e78cf
yjs2952 <yjs2952@naver.com>
2018-12-04 20:54:38 +0900

    자바스크립트 입출력, 제어구문

---
+++ javascriptStudy/ch07/7-4.js
@@ -0,0 +1,11 @@
+function fact(n) {
+    var k = 1, i = 1;
+    while (i < n) {
+        console.log(`i = ${i}, k = ${k}`);
+        k *= (++i);
+    }
+    console.log(`i = ${i}, k = ${k}`);
+    return k;
+}
+
+fact(5);
