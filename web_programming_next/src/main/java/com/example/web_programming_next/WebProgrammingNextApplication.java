656e70aab667e93cc8a06d8073950af537a34bca
yjs2952 <yjs2952@naver.com>
2018-12-07 19:04:30 +0900

    자바 웹 프로그래밍 Next Step 스터디 시작

---
+++ web_programming_next/src/main/java/com/example/web_programming_next/WebProgrammingNextApplication.java
@@ -0,0 +1,12 @@
+package com.example.web_programming_next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgrammingNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProgrammingNextApplication.class, args);
+	}
+}
