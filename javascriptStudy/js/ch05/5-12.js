2260823292552305e556789f1cb986feb7f1cedb
yjs <yjs2952@naver.com>
2018-12-18 15:16:35 +0900

    5장 함수

--- javascriptStudy/js/ch05/5-12.js
+++ javascriptStudy/js/ch05/5-12.js
@@ -1,5 +1,47 @@
-function sumAll() {
-    console.log(typeof (arguments) + ' : ' + arguments.length);
+// function sumAll() {
+//     console.log(typeof (arguments) + ' : ' + arguments.length);
+// }
+//
+// sumAll();
+
+// function pythagoras(width, height) {
+//     return Math.sqrt(width * width + height * height);
+// }
+
+// function square(x) {
+//     return x * x;
+// }
+//
+// function pythagoras(width, height) {
+//     return Math.sqrt(square(width) + square(height));
+// }
+
+/* 내가 만든거 */
+
+// 제곱 구하는 함수
+function square(x) {
+    return x * x;
 }
 
-sumAll();
+// 피타고라스
+function pythagoras(width, height) {
+
+// 제곱 구하는 함수
+    function square(x) {
+        return x * x;
+    }
+
+    return Math.sqrt(square(width) + square(height));
+}
+
+// 피타고라스 함수 호출
+console.log(pythagoras(3, 4));
+
+/* 니가 만든 함수 */
+// 삼각형이 직각인지 확인하는 함수
+function square(width, height, hypotenuse) {
+    if (width * width + height * height == hypotenuse * hypotenuse) {
+        return true;
+    }
+    return false;
+}
