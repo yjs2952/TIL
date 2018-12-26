46e1b21c4d45cf5b0fb470b1fdfe911721bda79b
yjs2952 <yjs2952@naver.com>
2018-12-26 21:21:49 +0900

    리액트 todo list 작성중

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,28 +1,120 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+import Pagination from './components/Pagination';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+
+        // an example array of 150 items to be paged
+        var exampleItems = [...Array(150).keys()].map(i => ({ id: (i+1), name: 'Item ' + (i+1) }));
+
+        this.state = {
+            exampleItems: exampleItems,
+            pageOfItems: []
+        };
+
+        // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
+        this.onChangePage = this.onChangePage.bind(this);
+    }
+
+    onChangePage(pageOfItems) {
+        // update state with new page of items
+        this.setState({ pageOfItems: pageOfItems });
+    }
+
+    id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+
+    state = {
+        input: '',
+        todos: [
+            {id: 0, text: ' 리액트 소개', checked: false},
+            {id: 1, text: ' 리액트 소개', checked: true},
+            {id: 2, text: ' 리액트 소개', checked: false}
+        ]
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            input: e.target.value // input 의 다음 바뀔 값
+        });
+    }
+
+    handleCreate = () => {
+        const {input, todos} = this.state;
+        this.setState({
+            input: '', // 인풋 비우고
+            // concat 을 사용하여 배열에 추가
+            todos: todos.concat({
+                id: this.id++,
+                text: input,
+                checked: false
+            })
+        });
+    }
+
+    handleKeyPress = (e) => {
+        // 눌려진 키가 Enter 면 handleCreate 호출
+        if (e.key === 'Enter') {
+            this.handleCreate();
+        }
+    }
+
+    handleToggle = (id) => {
+        const { todos } = this.state;
+
+        // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+        const index = todos.findIndex(todo => todo.id === id);
+        const selected = todos[index]; // 선택한 객체
+
+        const nextTodos = [...todos]; // 배열을 복사
+
+        // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+        nextTodos[index] = {
+            ...selected,
+            checked: !selected.checked
+        };
+
+        this.setState({
+            todos: nextTodos
+        });
+    }
+
+    handleRemove = (id) => {
+        const { todos } = this.state;
+        this.setState({
+            todos: todos.filter(todo => todo.id !== id)
+        });
+    }
+
+    render() {
+        const {input, todos} = this.state;
+        const {
+            handleChange,
+            handleCreate,
+            handleKeyPress,
+            handleToggle,
+            handleRemove
+        } = this;
+
+        return (
+            <TodoListTemplate form={(
+                <Form
+                    value={input}
+                    onKeyPress={handleKeyPress}
+                    onChange={handleChange}
+                    onCreate={handleCreate}
+                />
+            )}>
+                <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+                <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage} />
+            </TodoListTemplate>
+        );
+    }
 }
 
 export default App;
