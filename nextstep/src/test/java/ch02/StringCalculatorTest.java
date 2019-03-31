0a651a1af44ea4862b4393f257267f7e611017ef
yjs2952 <yjs2952@naver.com>
2019-03-31 20:53:03 +0900

    수정

--- nextstep/src/test/java/ch02/StringCalculatorTest.java
+++ nextstep/src/test/java/ch02/StringCalculatorTest.java
@@ -14,33 +14,33 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void _01문자열이_비었니() throws Exception {
+    public void _01_문자열이_비었니() throws Exception {
         Assert.assertEquals(0, sc.add(null));
         Assert.assertEquals(0, sc.add(""));
     }
 
     @Test
-    public void _02숫자하나입력() throws Exception {
+    public void _02_숫자하나입력() throws Exception {
         Assert.assertEquals(1, sc.add("1"));
     }
 
     @Test
-    public void _03숫자_두개_쉼표() throws Exception {
+    public void _03_숫자_두개_쉼표() throws Exception {
         Assert.assertEquals(3, sc.add("1,2"));
     }
 
     @Test
-    public void _04쉼표_이외_구분자() throws Exception {
+    public void _04_쉼표_이외_구분자() throws Exception {
         Assert.assertEquals(6, sc.add("1,2:3"));
     }
 
     @Test
-    public void _05문자사이_커스텀구분자() throws Exception {
+    public void _05_문자사이_커스텀구분자() throws Exception {
         Assert.assertEquals(6, sc.add("//;\n1;2;3"));
     }
 
     @Test(expected = RuntimeException.class)
-    public void _06음수_런타임에러() throws Exception {
+    public void _06_음수_런타임에러() throws Exception {
         sc.add("1,-2,3");
     }
 }
